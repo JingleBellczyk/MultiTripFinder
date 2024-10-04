72e0e39c13c597627d0cdb977401aa8f2dd8b713
user <voportno@gmail.com>
2024-10-04 13:35:36 +0200

    Fixed open api generation

--- backend/service/src/main/java/org/dyploma/userinfo/domain/UserInfoController.java
+++ backend/service/src/main/java/org/dyploma/userinfo/domain/UserInfoController.java
@@ -2,12 +2,14 @@ package org.dyploma.userinfo.domain;
 
 import com.openapi.api.UserApi;
 import com.openapi.model.UserCreation;
+import com.openapi.model.UserInfo;
 import com.openapi.model.UserInfoPage;
 import org.dyploma.userinfo.UserInfoService;
 import org.dyploma.userinfo.dto.UserInfoRequest;
 import org.dyploma.userinfo.dto.UserInfoResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ServerWebExchange;
 
 import java.util.List;
 import java.util.UUID;
@@ -18,25 +20,5 @@ public class UserInfoController implements UserApi {
 
     public UserInfoController(UserInfoService userInfoService) {
         this.userInfoService = userInfoService;
-    }
-
-    @Override
-    public ResponseEntity<com.openapi.model.UserInfo> createUser(UserCreation userCreation) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<com.openapi.model.UserInfo> findUser(String userId) {
-        return UserApi.super.findUser(userId);
-    }
-
-    @Override
-    public ResponseEntity<UserInfoPage> listUsers(String search, Integer page, Integer size, List<String> sort) {
-        return UserApi.super.listUsers(search, page, size, sort);
-    }
-
-    @Override
-    public ResponseEntity<Void> deleteUser(String userId) {
-        return UserApi.super.deleteUser(userId);
     }
 }
